b8f834c4dae256087a05405284eb4e60459fb118
Bozog <61922236+Bozog@users.noreply.github.com>
2020-06-20 18:32:10 +0100

    Update gcode.h

--- grbl/gcode.h
+++ grbl/gcode.h
@@ -117,8 +117,7 @@
 
 // Modal Group M8: Coolant control
 #define COOLANT_DISABLE 0 // M9 (Default: Must be zero)
-//#define COOLANT_FLOOD_ENABLE  PL_COND_FLAG_COOLANT_FLOOD // M8 (NOTE: Uses planner condition bit flag)
-#define COOLANT_FLOOD_ENABLE  0
+#define COOLANT_FLOOD_ENABLE  PL_COND_FLAG_COOLANT_FLOOD // M8 (NOTE: Uses planner condition bit flag)
 #define COOLANT_MIST_ENABLE   PL_COND_FLAG_COOLANT_MIST  // M7 (NOTE: Uses planner condition bit flag)
 
 // Modal Group G8: Tool length offset
